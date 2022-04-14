4e155507afa5d3232f08c538890a4a6d532c5078
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-14 02:46:18 +0000

    Created Bill Calculator

---
+++ Remaining Days.py
@@ -0,0 +1,6 @@
+age = input("What is your current age?")
+current_age=int(age)
+remaining_time = 90 - current_age
+print(f"you have {remaining_time*365} days, {remaining_time*52} weeks, and {remaining_time*12} months left")
+
+
---
+++ TipCalculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator")
+total_bill = float(input("What was the total bill?" ))
+tip= int(input("What percentage tip would you like to give? 10,12, or 15?"))
+no_of_people = int(input("How many people to split the bill?"))
+bill_plus_tip= total_bill + ((tip * total_bill)/100)
+each_person_pay = round((bill_plus_tip/no_of_people),2)
+print(f"Each person should pay: {each_person_pay}")
